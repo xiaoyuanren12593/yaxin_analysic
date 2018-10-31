bc65d0a37f31250dbd6e2311b699f139e94e63d9
xingyuan <13089011889@163.com>
2018-10-31 16:25:55 +0800

    test1

---
+++ src/main/java/com/yaxin/bigdata/test1.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C), 2015-2018, XXX 有限公司
+ * FileName: test1
+ * Author: Administrator
+ * Date: 2018-10-31 下午 4:24
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+package com.yaxin.bigdata;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
